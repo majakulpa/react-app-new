0100e401f8d66a686a012a1fa7f6a9fdb78f242e
majakulpa <majaweronikakulpa@gmail.com>
2020-02-13 23:33:32 +1100

    redux

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import Button from "./../../../components/UI/Button/Button";
 import styles from "./ContactData.module.css";
 import axios from "./../../../axios-orders";
@@ -90,7 +91,7 @@ class ContactData extends Component {
       ].value;
     }
     const order = {
-      ingredients: this.props.ingredients,
+      ingredients: this.props.ings,
       price: this.props.price,
       orderData: formData
     };
@@ -180,4 +181,11 @@ class ContactData extends Component {
   }
 }
 
-export default ContactData;
+const mapStoreToProps = state => {
+  return {
+    ings: state.ingredients,
+    price: state.totalPrice
+  };
+};
+
+export default connect(mapStoreToProps)(ContactData);
