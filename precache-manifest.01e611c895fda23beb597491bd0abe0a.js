78ca7a6fc2e573b41a7386150fe3bd59b2c721f8
majakulpa <majaweronikakulpa@gmail.com>
2020-03-01 13:14:09 +1100

    Updates

---
+++ precache-manifest.01e611c895fda23beb597491bd0abe0a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35e73c4a7ab4746d6c3cc912fef67ea4",
+    "url": "/react-app-new/index.html"
+  },
+  {
+    "revision": "14e559f0baf9c89d3eeb",
+    "url": "/react-app-new/static/css/3.02dda609.chunk.css"
+  },
+  {
+    "revision": "5ba368793f262962184e",
+    "url": "/react-app-new/static/css/4.bebde759.chunk.css"
+  },
+  {
+    "revision": "35edbe97a3f7a67b2169",
+    "url": "/react-app-new/static/css/5.19738895.chunk.css"
+  },
+  {
+    "revision": "8c88e6a421dda263ee81",
+    "url": "/react-app-new/static/css/main.f26f46d9.chunk.css"
+  },
+  {
+    "revision": "a629b846f55fbc449791",
+    "url": "/react-app-new/static/js/2.63b5d5ba.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-app-new/static/js/2.63b5d5ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "14e559f0baf9c89d3eeb",
+    "url": "/react-app-new/static/js/3.2f5234d3.chunk.js"
+  },
+  {
+    "revision": "5ba368793f262962184e",
+    "url": "/react-app-new/static/js/4.6417d624.chunk.js"
+  },
+  {
+    "revision": "35edbe97a3f7a67b2169",
+    "url": "/react-app-new/static/js/5.e3e90090.chunk.js"
+  },
+  {
+    "revision": "8c88e6a421dda263ee81",
+    "url": "/react-app-new/static/js/main.cab11be0.chunk.js"
+  },
+  {
+    "revision": "c2178eff87ec3f6d3c24",
+    "url": "/react-app-new/static/js/runtime-main.f2c3ae87.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/react-app-new/static/media/burger-logo.b8503d26.png"
+  }
+]);
