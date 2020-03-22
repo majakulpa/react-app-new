58eecca7fc936543c3fc62e718a3de4f52c226ee
majakulpa <majaweronikakulpa@gmail.com>
2020-03-22 18:39:36 +1100

    redux saga

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,10 +1,47 @@
 //star turning function into 'generator'
+import { delay } from "redux-saga/effects";
 import { put } from "redux-saga/effects";
-import * as actionTypes from "../actions/actionsTypes";
+import axios from "axios";
+import * as actions from "../actions/index";
 
 export function* logoutSaga(action) {
   yield localStorage.removeItem("token");
   yield localStorage.removeItem("expirationDate");
   yield localStorage.removeItem("userId");
-  yield put({ type: actionTypes.AUTH_LOGOUT });
+  yield put(actions.logoutSucceed());
+}
+
+export function* checkAuthTimeoutSaga(action) {
+  yield delay(action.expirationTime * 1000);
+  yield put(actions.logout());
+}
+
+export function* authUserSaga(action) {
+  yield put(actions.authStart());
+  const authData = {
+    email: action.email,
+    password: action.password,
+    returnSecureToken: true
+  };
+  let url =
+    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB-veoV1KMoALQi2dWACo3-plmxf7N1m8Y";
+  if (!action.isSignup) {
+    url =
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB-veoV1KMoALQi2dWACo3-plmxf7N1m8Y";
+  }
+  try {
+    const response = yield axios.post(url, authData);
+    const expirationDate = yield new Date(
+      new Date().getTime() + response.data.expiresIn * 1000
+    );
+    yield localStorage.setItem("token", response.data.idToken);
+    yield localStorage.setItem("expirationDate", expirationDate);
+    yield localStorage.setItem("userId", response.data.localId);
+    yield put(
+      actions.authSuccess(response.data.idToken, response.data.localId)
+    );
+    yield put(actions.checkAuthTimeout(response.data.expiresIn));
+  } catch (error) {
+    yield put(actions.authFail(error.response.data.error));
+  }
 }
